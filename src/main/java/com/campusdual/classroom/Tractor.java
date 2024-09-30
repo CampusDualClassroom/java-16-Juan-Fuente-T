dce009f83c30c947bd4d182bb6a1e2ffdc6a4a57
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-09-30 14:28:26 +0200

    Ejercicio 16 resuelto

--- src/main/java/com/campusdual/classroom/Tractor.java
+++ src/main/java/com/campusdual/classroom/Tractor.java
@@ -1,19 +1,39 @@
 package com.campusdual.classroom;
 
-public class Tractor{
+public class Tractor implements IMachine {
 
-	protected int horsePower = 0;
-	
-	public Tractor(int hp) {
-		this.horsePower = hp;
-	}
 
-	public void forward() {
-		System.out.println("El tractor avanza");
-	}
+    protected int horsePower;
 
-	public void backward() {
-		System.out.println("El tractor retrocede");
-	}
+    public Tractor(int hp) {
+        this.horsePower = hp;
+    }
+
+    public void forward() {
+        System.out.println("El tractor avanza");
+    }
+
+    public void backward() {
+        System.out.println("El tractor retrocede");
+    }
+
+    @Override
+    public void start() {
+        System.out.println("El tractor está encendido.");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("El tractor está apagado.");
+    }
+
+    @Override
+    public void maintenance() {
+        System.out.println("El tractor está realizando el mantenimiento.");
+    }
+
+    public void power() {
+        System.out.println("El tractor tiene " + this.horsePower + " hp de potencia.");
+    }
 
 }
